18ca7853c98702474973afc1d8dc1715c6ac5323
hareshp-ezdi <haresh.p@ezdi.us>
2018-04-23 17:04:50 +0530

    Finished 07 Directives Deep Dive

--- angular4/src/app/basic-highlighter/basic-highilghter-directive.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { BasicHighilghterDirectiveDirective } from './basic-highilghter-directive.directive';
-
-describe('BasicHighilghterDirectiveDirective', () => {
-  it('should create an instance', () => {
-    const directive = new BasicHighilghterDirectiveDirective();
-    expect(directive).toBeTruthy();
-  });
-});
