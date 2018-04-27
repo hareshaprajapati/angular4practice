00795740e36e7ed22898ec39ab518f41296a12ac
haresh-prajapati <hareshaprajapati@gmail.com>
2018-04-27 18:27:29 +0530

    start from 184 TD Using Form Data

--- Fifteen-Handling-Forms-in-Angular-Apps/src/app/app.component.ts
+++ Fifteen-Handling-Forms-in-Angular-Apps/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef} from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import {NgForm} from "@angular/forms";
 
 @Component({
@@ -7,11 +7,57 @@ import {NgForm} from "@angular/forms";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  suggestUserName() {
-    const suggestedName = 'Super asdf user';
+
+  @ViewChild('form') form: NgForm;
+  defaultQuestion = "pet";
+  answer: string;
+
+  genders = ['male', 'female'];
+  defaultGender = 'male';
+
+  user: {
+    'gender': string,
+    'questionAnswer': string,
+    'secret': string,
+    'userData': {
+      'email': string,
+      'username': string
+    }
   }
 
-  onSubmit(form : NgForm){
-    console.log(" form : " + form);
+  formSubmmited = false;
+
+  suggestUserName() {
+    const suggestedName = 'Super asdf user';
+    this.form.setValue(
+      {
+        'gender': 'male',
+        'questionAnswer': '',
+        'secret': "pet",
+        'userData': {
+          'email': "hareshaprajapati@gmail.com",
+          'username': "df"
+        }
+      }
+    );
+    /*this.form.form.patchValue({
+      'userData': {
+        'username': "df"
+      }
+    });*/
+
   }
+
+  onSubmit() {
+    this.user.userData.username = this.form.value.gender;
+    this.user.userData.email= this.form.value.email;
+    this.user.secret = this.form.value.secret;
+    this.user.gender = this.form.value.gender;
+    this.user.questionAnswer = this.form.value.questionAnswer;
+    this.formSubmmited = true;
+  }
+
+  /*onSubmit(form : NgForm){
+    console.log(form);
+  }*/
 }
